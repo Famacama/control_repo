5b3b027f27c08725809c440d5f3d4b05f76641b1
Famacama <59574182+Famacama@users.noreply.github.com>
2021-12-22 21:33:38 +0100

    Update ssh.pp

--- site/profile/manifests/ssh.pp
+++ site/profile/manifests/ssh.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCrhgU4SHKucoaW7SrIyZi9ptLmScS8RIZBna9uKO0uMkxrnpOAZSRTRMCBIg7S0U4XyhoSDjnz624uzguOFDKFtBiiesHrPgBOcZTmEvonjDXZACviQqRUqOQOT3+Q6z7HduoP/Q31GRykKkYDdk1wPqtH6qVwOE6zoI0u705aafoWX6+uY5jmLJvOv3xvu6Q2BXXQZPWIoHsRT89oihnUQq/lMEiettvbh4HmO4WJ8tNfwm6YJL85iayScEGMXcpHhjnoGNC/yXnC2aGCjXqqHrUjJen1fRuUtljQ7NUpQ8Nd8cSa8zbtZTp24YM40VmFxkPEpJKHfWT2+9FsKzp3 root@master.puppet.vm',
 	}  
 }
